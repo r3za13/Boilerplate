8e4caf781ab4eff09180d61e38d70ed981bc7da7
Reza <reza.salib@gmail.com>
2020-07-10 13:25:12 +0430

    init commit

---
+++ app/src/main/java/ir/r3za/boilerplate/MyApplication.kt
@@ -0,0 +1,11 @@
+package ir.r3za.boilerplate
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
